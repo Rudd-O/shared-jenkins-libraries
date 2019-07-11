9b31d454056a01d477a9df4d2ac1fe793d60193d
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2019-07-11 22:27:05 +0000

    Test this.

--- vars/cpanspecSrpm.groovy
+++ vars/cpanspecSrpm.groovy
@@ -1,7 +1,7 @@
 def call() {
     dir('src') {
         sh '''
-            pkg=$(echo "$JOB_NAME" | sed 's/^perl-//' | sed 's|/.+||' | sed 's/-/::/g')
+            pkg=$(echo "$JOB_NAME" | sed 's/^perl-//' | sed 's|/.*||' | sed 's/-/::/g')
             cpanspec -v -r $BUILD_NUMBER -s --packager "Generic RPM builder" "$pkg"
         '''
     }
