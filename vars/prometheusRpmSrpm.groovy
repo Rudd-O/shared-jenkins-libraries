2c3a1833cf5e977f53981492347cc5f1c2e4db22
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2019-07-15 23:24:24 +0000

    Add Prometheus-specific SRPM step.

---
+++ vars/prometheusRpmSrpm.groovy
@@ -0,0 +1,25 @@
+def f(pkgname, sha256sum) {
+	script {
+		dir("upstream") {
+			sh "sed -i 's|{{release}}|%{?build_number}%{?!build_number:{{release}}}|g' templates/spec.tpl"
+			sh "python2 ./generate.py --templates ${pkgname}"
+			sh """
+				sed -i 's|%{_unitdir}|/etc/systemd/system|g' ${pkgname}/autogen_${pkgname}.spec
+			"""
+		}
+		funcs.srpmFromSpecWithUrl(
+			"upstream/${pkgname}/autogen_${pkgname}.spec",
+			"upstream/${pkgname}",
+			"src",
+			sha256sum,
+		)()
+	}
+}
+
+def call(pkgname, sha256sum) {
+    return {
+        script {
+            f(pkgname, sha256sum)
+        }
+    }
+}
