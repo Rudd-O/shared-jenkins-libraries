1c7a07c1f568d3dcaf9afb5795ae5de2f62d0364
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2020-05-01 19:16:09 +0000

    Initialize RPM repo pipeline.

---
+++ vars/initializeRPMRepositoryPipeline.groovy
@@ -0,0 +1,54 @@
+def RELEASE = funcs.loadParameter('parameters.groovy', 'RELEASE', '30')
+
+def call() {
+	pipeline {
+
+		agent { label 'master' }
+
+		options {
+			disableConcurrentBuilds()
+			skipDefaultCheckout()
+			buildDiscarder(logRotator(numToKeepStr: '50', artifactNumToKeepStr: '1'))
+		}
+
+		parameters {
+			string defaultValue: '', description: "Which Fedora releases to build for (empty means the job's default).", name: 'RELEASE', trim: true
+		}
+
+		stages {
+			stage('Begin') {
+				steps {
+					script{
+						if (params.RELEASE == '') {
+							env.RELEASE = funcs.loadParameter('parameters.groovy', 'RELEASE', '30')
+						} else {
+							env.RELEASE = params.RELEASE
+						}
+					}
+					script {
+						funcs.announceBeginning()
+						funcs.durable()
+					}
+				}
+			}
+			stage('Create repository') {
+				steps {
+					lock('autouploadfedorarpms') {
+						script {
+							env.RELEASE.split(' ').each {
+								sh "/var/lib/jenkins/userContent/upload-deliverables --new ${it}"
+							}
+						}
+					}
+				}
+			}
+		}
+		post {
+			always {
+				script {
+					funcs.announceEnd(currentBuild.currentResult)
+				}
+			}
+		}
+	}
+}
