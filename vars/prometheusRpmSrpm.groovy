0d1759c03fff64e51553cb5da9d995939c66d9b7
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2020-05-02 06:37:50 +0000

    Prometheus RPMs to Python 3.

--- vars/prometheusRpmSrpm.groovy
+++ vars/prometheusRpmSrpm.groovy
@@ -2,7 +2,7 @@ def f(pkgname, sha256sum, armsha256sum='') {
 	script {
 		dir("upstream") {
 			sh "sed -i 's|{{release}}|%{?build_number}%{?!build_number:{{release}}}|g' templates/spec.tpl"
-			sh "python2 ./generate.py --templates ${pkgname}"
+			sh "python3 ./generate.py --templates ${pkgname}"
 			sh """
 				sed -i 's|%{_unitdir}|/etc/systemd/system|g' ${pkgname}/autogen_${pkgname}.spec
 			"""
