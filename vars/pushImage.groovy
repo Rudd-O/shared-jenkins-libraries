206af8634df1c9dbb85f9d5d15672f6028dd39e2
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-02-07 15:45:38 +0000

    tags.

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -3,6 +3,6 @@ def call(String imageTag) {
 	def server = "docker.dragonfear:80"
 	def regsources = '{"insecureRegistries": ["' + server + '"]}'
 	withEnv(["BUILD_REGISTRY_SOURCES=${regsources}"]) {
-		return sh "buildah push ${imageTag} docker://${server}/${imageTag}"
+		sh "buildah push ${imageTag} docker://${server}/${imageTag}"
 	}
 }
