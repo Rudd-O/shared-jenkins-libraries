4f9dd8652703a0085b06c96f4493f9239d528ef2
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-02-07 16:22:23 +0000

    Secrets for pusher.

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,8 +1,18 @@
 def call(String imageTag) {
 	// """Push docker image to server."""
-	def server = "docker.dragonfear:80"
-	def regsources = '{"insecureRegistries": ["' + server + '"]}'
-	withEnv(["BUILD_REGISTRY_SOURCES=${regsources}"]) {
-		sh "buildah push ${imageTag} docker://${server}/${imageTag}"
+	withEnv("REGISTRY_AUTH_FILE=.regauth", "imagetag=${imageTag}"]) {
+		withCredentials([
+			usernamePassword(credentialsId: 'docker-auth', usernameVariable: 'docker_username', passwordVariable: 'docker_password'),
+			string(credentialsId: 'docker-server', variable: 'docker_server'),
+		]) {
+			sh 'env'
+			def regsources = '{"insecureRegistries": ["' + env.docker_server + '"]}'
+			withEnv(["BUILD_REGISTRY_SOURCES=${regsources}"]) {
+				sh '''
+				buildah login -u "$docker_username" -p "$docker_password" "$docker_server"
+				buildah push "$imagetag" "docker://$docker_server/$imagetag"
+				'''
+			}
+		}
 	}
 }
