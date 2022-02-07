2986ac36ea9b8384271c5ca2bbf55f08b12004e6
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-02-07 13:40:02 +0000

    Fix docker pipeline initial.

--- vars/genericDockerPipeline.groovy
+++ vars/genericDockerPipeline.groovy
@@ -131,88 +131,88 @@ def call(build_deps = null, test_step = null) {
 			//		unstash 'out'
 			//	}
 			//}
-			/*stage('Sign') {
-				steps {
-					sh '''#!/bin/bash -e
-					olddir="$PWD"
-					cd "$JENKINS_HOME"
-					PRIVKEY=
-					if test -d rpm-sign ; then
-					  for f in rpm-sign/RPM-GPG-KEY-*.private.asc ; do
-					    if test -f "$f" ; then
-					      PRIVKEY="$PWD"/"$f"
-					      break
-					    fi
-					  done
-					fi
-					if [ "$PRIVKEY" == "" ] ; then
-					  >&2 echo error: could not find PRIVKEY in rpm-sign/, aborting
-					  exit 40
-					fi
-					cd "$olddir"
-					sign() {
-					  if [ -z "$tmpdir" ] ; then
-					    tmpdir=$(mktemp -d)
-					    trap 'echo rm -rf "$tmpdir"' EXIT
-					  fi
-					  export GNUPGHOME="$tmpdir"
-					  errout=$(gpg2 --import < "$PRIVKEY" 2>&1) || {
-					    ret=$?
-					    >&2 echo "$errout"
-					    return $ret
-					  }
-					  GPG_NAME=$( gpg2 --list-keys | egrep '^      ([ABCDEF0-9])*$' | head -1 )
-					  >&2 echo "Signing package $1."
-					  errout=$(rpm --addsign \
-					    --define "%_gpg_name $GPG_NAME" \
-					    --define '_signature gpg' \
-					    --define '_gpgbin /usr/bin/gpg2' \
-					    --define '__gpg_sign_cmd %{__gpg} gpg --force-v3-sigs --batch --verbose --no-armor --no-secmem-warning -u "%{_gpg_name}" -sbo %{__signature_filename} --digest-algo sha256 %{__plaintext_filename}' \
-					    "$1" 2>&1) || {
-					    ret=$?
-					    >&2 echo "$errout"
-					    return $ret
-					  }
-					  rpm -K "$1" || true
-					  rpm -q --qf '%{SIGPGP:pgpsig} %{SIGGPG:pgpsig}\n' -p "$1"
-					}
-					for rpm in out/*/*.rpm ; do
-					  sign "$rpm"
-					done
-					'''
-				}
-			}
-			stage('Archive') {
-				steps {
-					archiveArtifacts artifacts: 'out/*/*.rpm', fingerprint: true
-				}
-			}
-			stage('Integration') {
-				when {
-					expression {
-						return integration_step != null
-					}
-				}
-				steps {
-					script {
-						integration_step()
-					}
-				}
-			}
-			stage('Publish') {
-				when {
-					expression {
-						return env.BRANCH_NAME == "master"
-					}
-				}
-				steps {
-					lock('autouploadfedorarpms') {
-						script {
-							autouploadfedorarpms()
-						}
-					}
-				}
-			}*/
+			//stage('Sign') {
+			//	steps {
+			//		sh '''#!/bin/bash -e
+			//		olddir="$PWD"
+			//		cd "$JENKINS_HOME"
+			//		PRIVKEY=
+			//		if test -d rpm-sign ; then
+			//		  for f in rpm-sign/RPM-GPG-KEY-*.private.asc ; do
+			//		    if test -f "$f" ; then
+			//		      PRIVKEY="$PWD"/"$f"
+			//		      break
+			//		    fi
+			//		  done
+			//		fi
+			//		if [ "$PRIVKEY" == "" ] ; then
+			//		  >&2 echo error: could not find PRIVKEY in rpm-sign/, aborting
+			//		  exit 40
+			//		fi
+			//		cd "$olddir"
+			//		sign() {
+			//		  if [ -z "$tmpdir" ] ; then
+			//		    tmpdir=$(mktemp -d)
+			//		    trap 'echo rm -rf "$tmpdir"' EXIT
+			//		  fi
+			//		  export GNUPGHOME="$tmpdir"
+			//		  errout=$(gpg2 --import < "$PRIVKEY" 2>&1) || {
+			//		    ret=$?
+			//		    >&2 echo "$errout"
+			//		    return $ret
+			//		  }
+			//		  GPG_NAME=$( gpg2 --list-keys | egrep '^      ([ABCDEF0-9])*$' | head -1 )
+			//		  >&2 echo "Signing package $1."
+			//		  errout=$(rpm --addsign \
+			//		    --define "%_gpg_name $GPG_NAME" \
+			//		    --define '_signature gpg' \
+			//		    --define '_gpgbin /usr/bin/gpg2' \
+			//		    --define '__gpg_sign_cmd %{__gpg} gpg --force-v3-sigs --batch --verbose --no-armor --no-secmem-warning -u "%{_gpg_name}" -sbo %{__signature_filename} --digest-algo sha256 %{__plaintext_filename}' \
+			//		    "$1" 2>&1) || {
+			//		    ret=$?
+			//		    >&2 echo "$errout"
+			//		    return $ret
+			//		  }
+			//		  rpm -K "$1" || true
+			//		  rpm -q --qf '%{SIGPGP:pgpsig} %{SIGGPG:pgpsig}\n' -p "$1"
+			//		}
+			//		for rpm in out/*/*.rpm ; do
+			//		  sign "$rpm"
+			//		done
+			//		'''
+			//	}
+			//}
+			//stage('Archive') {
+			//	steps {
+			//		archiveArtifacts artifacts: 'out/*/*.rpm', fingerprint: true
+			//	}
+			//}
+			//stage('Integration') {
+			//	when {
+			//		expression {
+			//			return integration_step != null
+			//		}
+			//	}
+			//	steps {
+			//		script {
+			//			integration_step()
+			//		}
+			//	}
+			//}
+			//stage('Publish') {
+			//	when {
+			//		expression {
+			//			return env.BRANCH_NAME == "master"
+			//		}
+			//	}
+			//	steps {
+			//		lock('autouploadfedorarpms') {
+			//			script {
+			//				autouploadfedorarpms()
+			//			}
+			//		}
+			//	}
+			//}*/
 		}
 		post {
 			always {
