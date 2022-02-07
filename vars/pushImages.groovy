816ff6ce2dbd5a50f8a3a4314bcc226972518375
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-02-07 17:06:24 +0000

    Fasten 2.

--- vars/pushImages.groovy
+++ vars/pushImages.groovy
@@ -1,6 +1,6 @@
 def call(String[] imageTags) {
 	// """Push docker images to server."""
-	withEnv(["REGISTRY_AUTH_FILE=.regauth", "imagetag=${imageTag}"]) {
+	withEnv(["REGISTRY_AUTH_FILE=.regauth"]) {
 		withCredentials([
 			usernamePassword(credentialsId: 'docker-auth', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD'),
 			string(credentialsId: 'docker-server', variable: 'DOCKER_SERVER'),
@@ -9,7 +9,9 @@ def call(String[] imageTags) {
 			withEnv(["BUILD_REGISTRY_SOURCES=${regsources}"]) {
 				sh 'buildah login --tls-verify=false -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" "$DOCKER_SERVER"'
 				for (imageTag in imageTags) {
-					sh 'buildah push --tls-verify=false "$imagetag" "docker://$DOCKER_SERVER/$imagetag"'
+					withEnv(["imagetag=${imageTag}"]) {
+						sh 'buildah push --tls-verify=false "$imagetag" "docker://$DOCKER_SERVER/$imagetag"'
+					}
 				}
 			}
 		}
