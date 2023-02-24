6794f520815c8918329f68b1d869c68fc0ceb4bf
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2023-02-24 13:20:43 +0000

    Use config map.

--- vars/srpmFromSpecWithUrl.groovy
+++ vars/srpmFromSpecWithUrl.groovy
@@ -1,18 +1,21 @@
-def call(String specfilename, String srcdir, String outdir, String sha256sum='', String armsha256sum='') {
-	// Specfile SOURCE0 has the URL.  Sha256sum validates the URL's ontents.
-	// srcdir is where the URL file is deposited.
-	// outdir is where the source RPM is deposited.  It is customarily src/ cos that's where automockrpms finds it.
-        url = funcs.getrpmfield(specfilename, "Source0")
+def call(Map config) {
+        // config.specfile is a file where SOURCE0 has the URL.
+        // config.srcdir is where the URL file is deposited.
+        // config.outdir is where the source RPM is deposited.  It is customarily src/ cos that's where automockrpms finds it.
+        // config.sha256sum validates the URL's contents.
+        // config.armsha256sum = '' validates the ARM URL's contents.
+        // Specfile
+        url = funcs.getrpmfield(config.specfile, "Source0")
         if (url == "") {
                 error("Could not compute URL of source tarball.")
         }
-        funcs.downloadUrl(url, null, sha256sum, srcdir)
-        if (armsha256sum != ''){
+        funcs.downloadUrl(url, null, config.sha256sum, config.srcdir)
+        if (config.containsKey("armsha256sum")){
                 armurl = url.replace("amd64", "arm64")
-                funcs.downloadUrl(armurl, null, armsha256sum, srcdir)
-                name = funcs.getrpmfield(specfilename, "Name")
-                version = funcs.getrpmfield(specfilename, "Version")
-                dir(srcdir) {
+                funcs.downloadUrl(armurl, null, config.armsha256sum, config.srcdir)
+                name = funcs.getrpmfield(config.specfile, "Name")
+                version = funcs.getrpmfield(config.specfile, "Version")
+                dir(config.srcdir) {
                         sh """
                         tar zxvmf \$(basename ${url})
                         rm -f \$(basename ${url})
@@ -26,7 +29,7 @@ def call(String specfilename, String srcdir, String outdir, String sha256sum='',
                         tar ztvmf \$(basename ${url})
                         """
                 }
-                sh "sed -i 's|%setup -q -n %{name}-%{version}.linux-amd64|%setup -q -n %{name}-%{version}.linux-%{_target_cpu}|' ${specfilename}"
+                sh "sed -i 's|%setup -q -n %{name}-%{version}.linux-amd64|%setup -q -n %{name}-%{version}.linux-%{_target_cpu}|' ${config.specfile}"
         }
-        sh "rpmbuild --define \"_srcrpmdir ${outdir}\" --define \"_sourcedir ${srcdir}\" -bs ${specfilename}"
+        sh "rpmbuild --define \"_srcrpmdir ${config.outdir}\" --define \"_sourcedir ${config.srcdir}\" -bs ${config.specfile}"
 }
