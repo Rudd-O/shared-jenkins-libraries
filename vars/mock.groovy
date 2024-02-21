b46d86857d22ae172521494382e61e0a8b4de534
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2024-02-21 16:06:18 +0000

    Redoce debuglevel to 0.

--- vars/mock.groovy
+++ vars/mock.groovy
@@ -46,6 +46,7 @@ config_opts['plugin_conf']['root_cache_opts']['exclude_dirs'] = ['./proc', './sy
 config_opts['dnf.conf'] = """
 [main]
 keepcache=1
+debuglevel=0
 cachedir=/var/cache/yum
 reposdir=/dev/null
 logfile=/var/log/yum.log
@@ -133,7 +134,7 @@ config_opts['package_manager'] = 'dnf'
 config_opts['dnf.conf'] = """
 [main]
 keepcache=1
-debuglevel=1
+debuglevel=0
 reposdir=/dev/null
 logfile=/var/log/yum.log
 retries=20
