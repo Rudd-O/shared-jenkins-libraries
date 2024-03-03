ac4ad7c052c5fa707579e8225f98010ccf6b59e8
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2024-03-03 21:13:27 +0000

    Do not fail announce.

--- vars/announceEnd.groovy
+++ vars/announceEnd.groovy
@@ -1,7 +1,7 @@
 def call(String status) {
     sh(
         script: """#!/bin/bash
-        "$JENKINS_HOME"/userContent/announce-build-result finished ${status}
+        "$JENKINS_HOME"/userContent/announce-build-result finished ${status} || true
         """,
         label: "Announce build end"
     )
