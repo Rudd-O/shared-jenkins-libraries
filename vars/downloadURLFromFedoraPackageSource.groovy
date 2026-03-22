9a2f0d637b7aa8dbe69f89ecfd6869ddc934e9e7
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2026-03-22 12:17:28 +0000

    Add some defs.

--- vars/downloadURLFromFedoraPackageSource.groovy
+++ vars/downloadURLFromFedoraPackageSource.groovy
@@ -1,16 +1,16 @@
 def call(Map config) {
         // config.sha256sum validates the URL's contents.
-        specfile = null
+        def specfile = null
         if (config != null) {
                 specfile = config.specfile
         }
         if (specfile == null) {
                 specfile = funcs.findSpecfile()
         }
-        url = funcs.getrpmsources(specfile)[0]
-        fn = funcs.basename(url)
-        sum = ""
-        prefix = "SHA512 ($fn) = "
+        def url = funcs.getrpmsources(specfile)[0]
+        def fn = funcs.basename(url)
+        def sum = ""
+        def prefix = "SHA512 ($fn) = "
         for (line in readFile("sources").split("\n")) {
                 if (line.startsWith(prefix)) {
                         sum = line.minus(prefix)
